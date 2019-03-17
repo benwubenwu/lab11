902264d8c6836603b53a0b8c1e0d8080fb0985e4
Ben Wu <benjaminwu@college.harvard.edu>
2019-03-17 11:03:16 +0800

    lab 11

--- lab11.ml
+++ lab11.ml
@@ -34,19 +34,20 @@ let f1 b = if b then () else () ;;
 
 (* 2. f2 : int -> int option *)
 
-let f2 _ = failwith "f2 not implemented" ;;
+let f2 _ =   let _ = x + 1 in
+  Some 3 ;;
 
 (* 3. f3 : 'a * bool -> 'a *)
 
-let f3 _ = failwith "f3 not implemented" ;;
+let f3 (x, b) = if b then x else x ;;
   
 (* 4. f4 : 'a -> 'b list option *)
 
-let f4 _ = failwith "f4 not implemented" ;;
+let f4 x = Some [] ;;
 
 (* 5. f5: ('a -> 'b) -> ('b -> 'c) -> ('a -> 'c) *)
 
-let f5 _ = failwith "f5 not implemented" ;;
+let f5 f g x = g (f x) ;;
 
 (*....................................................................
 Exercise 2. For each of the following top-level let definitions, give
@@ -79,20 +80,20 @@ let x = let y x = x in y ;;
 
 let x3 = 3, 3 * 3, 3 ;; 
 
-(* place a typing for x2 here *)
+(x : 'a -> 'a) ;;
 
 (* 4. *)
 
 let x4 = List.map ((+) 3.) [4.; 5.; 6.] ;;
 
-(* place a typing for x3 here *)
+(x3 : int * int * int) ;;
 
 (* 5. *)
 
 let x5 = let open List in
         fun y -> filter ((=) y) ;;
 
-(* place a typing for x5 here *)
+(x5 : 'a -> 'a list -> 'a list) ;;
 
 (*====================================================================
 Part 2. Defining some functions
@@ -110,7 +111,8 @@ Exercise 4. Provide your own definition of the function string_of_bool
 (without using Pervasives.string_of_bool of course).
 ....................................................................*)
 
-let string_of_bool _ = failwith "string_of_bool not implemented" ;;
+let string_of_bool (b : bool) : string = 
+  if b then "true" else "false" ;;
 
 (*....................................................................
 Exercise 3. What is the type of string_of_bool? Give it as a typing,
@@ -119,7 +121,7 @@ like
     (string_of_bool : ...) ;;
 ....................................................................*)
          
-(* place your typing here *)
+(string_of_bool : bool -> string) ;;
 
 (* Recall that the Pervasives.compare function compares two values,
 returning an int based on their relative magnitude: compare x y
@@ -156,7 +158,8 @@ a full derivation of its complexity.
 ....................................................................*)
 
 (* place your explanation in this comment:
-
+  Has to go through all of length of the longer list before comparing the 2,
+  thus O(n) complexity.
    ...
  *)
 
@@ -166,7 +169,12 @@ whose complexity is O(n) where n is the length of the shorter of the
 two lists, not the longer.
 ....................................................................*)
 
-let compare_lengths _ = failwith "compare_lengths not implemented" ;;
+let rec compare_lengths xs ys =
+  match xs, ys with
+  | [], [] -> 0
+  | _, [] -> 1
+  | [], _ -> -1
+  | _xhd :: xtl, _yhd :: ytl -> compare_lengths xtl ytl ;;
 
 (*====================================================================
 Part 3. Substitution semantics
@@ -181,10 +189,15 @@ evaluates to 5 according to the substitution semantics rules in the
 textbook.
 ....................................................................*)
 
-(* place your derivation in this comment here:
-
-     ....
-     
+(*
+    fun x -> x + 2 =>
+                   |  (fun x -> x + 2) => (fun x -> x + 2)
+                   |  3 => 3
+                   |  3 + 2 =>
+                            |  3 => 3
+                            |  2 => 2
+                            => 5
+                   => 5
  *)
 
 (*====================================================================
@@ -240,7 +253,14 @@ fixed.
 Exercise 8a. Now correct the code above for the insert function.
 ....................................................................*)
 
-let insert _ = failwith "insert not implemented" ;;
+let rec insert (item : 'a) (tree : 'a bintree) : 'a bintree =
+  match tree with
+  | Empty -> Node (Empty, item, Empty)
+  | Node (left, old, right) ->
+     if item < old then
+       Node (insert item left, old, right)
+     else
+       Node (left, old, insert item right) ;;
      
 (* Using the insert function (as corrected), we can, for instance,
 build a tree containing 10, 5, 15, 7, and 9, which looks like
@@ -302,7 +322,15 @@ occur in the tree. For instance,
     Exception: Failure "gorn: item not found".
 ....................................................................*)
 
-let gorn _ = failwith "gorn not implemented" ;;
+let rec gorn item tree =
+  match tree with
+  | Empty -> failwith "gorn: item not found"
+  | Node (left, old, right) ->
+     if item = old then []
+     else if item < old then
+       Left :: gorn item left
+     else
+       Right :: gorn item right ;;
 
 (*....................................................................
 Exercise 10. Define a function find : 'a -> 'a bintree -> bool that
@@ -316,7 +344,12 @@ defining find. Examples of the find function in use include:
     - : bool = false
 ....................................................................*)  
 
-let find _ = failwith "find not implemented" ;;
+let find item tree =
+  try
+    let _ = gorn item tree
+    in true
+  with
+  | Failure _ -> false ;;
 
 (*
                               END OF LAB
